ef2ae7494f23cc4fad995572317bbd6b4f3e5c6f
SantiagoFlorido <154099674+SantiagoFlorido@users.noreply.github.com>
2023-12-18 12:02:35 -0500

    Update database.js

--- src/utils/database.js
+++ src/utils/database.js
@@ -7,6 +7,14 @@ const db = new Sequelize({
     password: config.db.pass,
     database: config.db.name,
     port: config.db.port,
+    dialectOptions: 
+        process.env.NODE_ENV === 'production'
+        ? {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        } : {}
 })
 
 module.exports = db
